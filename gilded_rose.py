50565450860880911171e42f60908a57d3b4979a
Elliot Charney <elliot.charney@ithaka.org>
2017-05-09 12:19:44 -0400

    create vars for brie and sulfuras

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,23 +1,24 @@
 CONCERT = "Backstage passes to a TAFKAL80ETC concert"
-
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+BRIE = "Aged Brie"
 
 class GildedRose:
     @staticmethod
     def update_quality(items):
         for i in range(0, len(items)):
-            if "Aged Brie" != items[i].name and CONCERT != items[i].name:
+            if BRIE != items[i].name and CONCERT != items[i].name:
                 # TODO: Improve this code.  Word.
                 if items[i].quality > 0:
-                    if "Sulfuras, Hand of Ragnaros" != items[i].name:
+                    if SULFURAS != items[i].name:
                         items[i].quality = items[i].quality - 1
             else:
                 if items[i].quality < 50:
                     items[i].quality = items[i].quality + 1
-                    if "Aged Brie" == items[i].name:
+                    if BRIE == items[i].name:
                         if items[i].sell_in < 6:
                             items[i].quality = items[i].quality + 1
                     # Increases the Quality of the stinky cheese if it's 11 days to due date.
-                    if "Aged Brie" == items[i].name:
+                    if BRIE == items[i].name:
                         if items[i].sell_in < 11:
                             items[i].quality = items[i].quality + 1
                     if CONCERT == items[i].name:
@@ -29,13 +30,13 @@ class GildedRose:
                         if items[i].sell_in < 6:
                             if items[i].quality < 50:
                                 items[i].quality = items[i].quality + 1
-            if "Sulfuras, Hand of Ragnaros" != items[i].name:
+            if SULFURAS != items[i].name:
                 items[i].sell_in = items[i].sell_in - 1
             if items[i].sell_in < 0:
-                if "Aged Brie" != items[i].name:
+                if BRIE != items[i].name:
                     if CONCERT != items[i].name:
                         if items[i].quality > 0:
-                            if "Sulfuras, Hand of Ragnaros" != items[i].name:
+                            if SULFURAS != items[i].name:
                                 items[i].quality = items[i].quality - 1
                     else:
                         # TODO: Fix this.
@@ -43,10 +44,10 @@ class GildedRose:
                 else:
                     if items[i].quality < 50:
                         items[i].quality = items[i].quality + 1
-                    if "Aged Brie" == items[i].name and items[i].sell_in <= 0:
+                    if BRIE == items[i].name and items[i].sell_in <= 0:
                         items[i].quality = 0
                         # of for.
-            if "Sulfuras, Hand of Ragnaros" != items[i].name:
+            if SULFURAS != items[i].name:
                 if items[i].quality > 50:
                     items[i].quality = 50
         return items
