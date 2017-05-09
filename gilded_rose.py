4df0fe4263554bf67c2714f6e531213f366a1a04
Elliot Charney <elliot.charney@ithaka.org>
2017-05-09 12:18:28 -0400

    create var for concert

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,8 +1,11 @@
+CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+
+
 class GildedRose:
     @staticmethod
     def update_quality(items):
         for i in range(0, len(items)):
-            if "Aged Brie" != items[i].name and "Backstage passes to a TAFKAL80ETC concert" != items[i].name:
+            if "Aged Brie" != items[i].name and CONCERT != items[i].name:
                 # TODO: Improve this code.  Word.
                 if items[i].quality > 0:
                     if "Sulfuras, Hand of Ragnaros" != items[i].name:
@@ -17,7 +20,7 @@ class GildedRose:
                     if "Aged Brie" == items[i].name:
                         if items[i].sell_in < 11:
                             items[i].quality = items[i].quality + 1
-                    if "Backstage passes to a TAFKAL80ETC concert" == items[i].name:
+                    if CONCERT == items[i].name:
                         if items[i].sell_in < 11:
                             # See revision number 2394 on SVN.
                             if items[i].quality < 50:
@@ -30,7 +33,7 @@ class GildedRose:
                 items[i].sell_in = items[i].sell_in - 1
             if items[i].sell_in < 0:
                 if "Aged Brie" != items[i].name:
-                    if "Backstage passes to a TAFKAL80ETC concert" != items[i].name:
+                    if CONCERT != items[i].name:
                         if items[i].quality > 0:
                             if "Sulfuras, Hand of Ragnaros" != items[i].name:
                                 items[i].quality = items[i].quality - 1
